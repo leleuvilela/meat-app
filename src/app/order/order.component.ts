bb1e5f63c22a3836ada0f1f4e8b0c9ded9ab2bd9
Vinícius Vilela <viniciusvilela19@gmail.com>
2018-02-19 11:42:53 -0300

    okey

---
+++ src/app/order/order.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from 'app/order/order.service';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { Order, OrderItem } from 'app/order/order.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  delivery: number = 8
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: "MON"},
+    {label: 'Cartão Refeição', value: "REF"},
+    {label: 'Cartão de Débito', value: "DEB"}
+  ]
+
+  constructor(private orderService: OrderService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.removeItem(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+      .map((item:CartItem)=>new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe((orderId: string) => {
+      this.router.navigate(['/order-summary'])
+      this.orderService.clear()
+    })
+    console.log(order)
+  }
+  
+
+}
